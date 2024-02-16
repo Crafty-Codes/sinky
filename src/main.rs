b352a437fde6e671930dcafd873f38fa0cad28de
Crafty-Codes <fabio.oberhofer@gmx.net>
2024-02-16 22:30:21 +0100

    showing image of boat on screen

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use bevy::prelude::*;
-use bevy::window::PrimaryWindow;
-
-fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
-}
 
 #[derive(Component)]
-pub struct Ship {}
+struct Ship;
+
+fn spawn_ship(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("sprites/boat.png"),
+            transform: Transform::from_translation(Vec3::new(20., 20., 2.)),
+            ..default()
+        },
+        Ship,
+    ));
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_ship)
+        .run();
+}
